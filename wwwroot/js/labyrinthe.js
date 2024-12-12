ef48270032233fa2660cdb33d7a62db4b9fafc13
user141241 <qaaa6757@gmail.com>
2024-12-12 15:01:29 +0100

    Add files via upload

---
+++ wwwroot/js/labyrinthe.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("keydown", function (event) {
+    const directions = {
+        "ArrowUp": "Top",
+        "z": "Top",
+        "ArrowDown": "BOTTOM",
+        "s": "BOTTOM",
+        "ArrowLeft": "LEFT",
+        "q": "LEFT",
+        "ArrowRight": "RIGHT",
+        "d": "RIGHT"
+    };
+
+    const direction = directions[event.key];
+
+    if (!direction) return;
+
+    $.ajax({
+        url: `/Labyrinthe/MovePlayer/Player1/${direction}`,
+        type: 'POST',
+        success: function () {
+            console.log("Direction envoyée :", direction);
+            location.reload();
+        },
+        error: function () {
+            console.error("Erreur lors du déplacement du joueur.");
+        }
+    });
+
+});
